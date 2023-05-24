fa06476fdcf581f7629f931beb73b910adfffa35
Horean0574 <Horean_ybc@163.com>
2023-05-24 13:48:07 +0800

    update Signup page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,7 +4,10 @@ import Layout from "../components/Layout";
 function Signup() {
     return (
         <Layout headline={"RC社团报名"}>
-
+            <iframe src="http://rc.horean0574.xyz" frameBorder="0" title={"RC社报名问卷星"} style={{
+                width: "100%",
+                height: "100%",
+            }}></iframe>
         </Layout>
     );
 }
