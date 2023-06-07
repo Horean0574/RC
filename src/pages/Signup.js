a1d8845b28055b20a427094c74256e3ef2e315c2
Horean0574 <Horean0574@outlook.com>
2023-06-07 17:02:10 +0800

    update signup page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -5,7 +5,7 @@ function Signup() {
     return (
         <Layout headline={"RC社团报名"}>
             <iframe style={{
-                height: "100%",
+                minHeight: "100%",
             }} id="goldendata_form_x3zLor"
                     src="https://jinshuju.net/f/x3zLor?background=white&banner=show&embedded=true&inner_redirect=false"
                     width="100%" frameborder="0" allowTransparency="true"/>
